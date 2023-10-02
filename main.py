3c78d09c242b1e6dbd2f011dc355179b49df9137
Genie <syi06047@gmail.com>
2023-10-02 23:16:22 +0900

    Update 2 on Github only

--- main.py
+++ main.py
@@ -21,3 +21,6 @@ print("Hello World Update on Github")
 
 #Update on local main branch
 print("Hello World Update on Local")
+
+#Update on github main branch
+print("Hello World Update 2 on Github only")
